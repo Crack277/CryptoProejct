b73b2dc6bdd29b4b84d1c5a147e1aaaaf9cd9236
redun <redunsasha2017>
2025-12-28 21:59:23 +0700

    add fields for api

--- core/config.py
+++ core/config.py
@@ -1,10 +1,17 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class CurrencyApi(BaseModel):
+    prefix: str = "/v1/cryptocurrency"
+    CMC_API_KEY: str
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         case_sensitive=False,
+        env_nested_delimiter="__",
     )
 
     db_username: str
@@ -13,6 +20,7 @@ class Settings(BaseSettings):
     db_port: int
     db_name: str
     db_echo: bool
+    api_key: CurrencyApi
 
     @property
     def db_url(self):
