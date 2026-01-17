e9c7dfac4cdf007396358da7fcf29bdb80dd1f59
redun <redunsasha2017@gmail.com>
2026-01-17 18:15:31 +0700

    edit fields in schemas

--- api_v1/models/schemas.py
+++ api_v1/models/schemas.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel, ConfigDict
 
 class CryptoBase(BaseModel):
     name: str
-    symbol: str
     price: float
-    last_updated: str
+    percent_change_24h: float
+    market_cap: float
 
 class Crypto(CryptoBase):
     model_config = ConfigDict(from_attributes=True)
