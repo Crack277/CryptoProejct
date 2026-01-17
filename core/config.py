0107448b68c59edefb92cc1a17003920250d4413
redun <redunsasha2017@gmail.com>
2026-01-17 18:18:07 +0700

    edit fields

--- core/config.py
+++ core/config.py
@@ -3,13 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class CurrencyApi(BaseModel):
-    prefix: str = "/v1/cryptocurrency"
+    prefix: str = "/v1"
     CMC_API_KEY: str
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=".env_example",
         case_sensitive=False,
         env_nested_delimiter="__",
     )
